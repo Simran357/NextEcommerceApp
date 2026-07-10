a38d3fe13b32ba0ad6a3268a11214616a02b3c9c
Simran357 <lovelysharma2535@gmail.com>
2026-07-10 10:52:56 +0530

    change the checkout app file

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import CheckoutClient from "@/components/cart/checkout";
+import { Suspense } from "react";
 
 export default function CheckoutPage() {
-  return <CheckoutClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutClient />
+    </Suspense>
+  );
 }
