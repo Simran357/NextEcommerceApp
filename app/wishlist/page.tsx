421b817e33b889d09e57792e5269d652a31cab29
Simran357 <lovelysharma2535@gmail.com>
2026-07-10 11:23:37 +0530

    wishlist page imoproved

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,24 +1,7 @@
-import WishlistPage from "@/components/wishlist/wishlistPage";
+"use client";
 
-export default async function Wishlist() {
+import  WishlistPage from "@/components/wishlist/wishlistPage";
 
-  return (
-    <div className="space-y-8">
-      <div>
-        <p className="text-sm font-semibold uppercase tracking-widest text-indigo-600">
-          Administration
-        </p>
-
-        <h1 className="mt-2 text-4xl font-bold">
-          Wishlists
-        </h1>
-z
-        <p className="mt-2 text-slate-500">
-          Products customers have saved.
-        </p>
-      </div>
-
-      <WishlistPage />
-    </div>
-  );
+export default function wishlist() {
+  return <WishlistPage />;
 }
